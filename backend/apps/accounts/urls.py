6f330c39d0f958ac4552e160412fe0dae4548fec
Shifana Barveen S <265015889+shifana21@users.noreply.github.com>
2026-09-11 11:48:39 +0530

    Add JWT authentication

--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
-from .views import RegisterView
+from .views import RegisterView, MeView
 
 
 urlpatterns = [
@@ -9,4 +10,20 @@ urlpatterns = [
         RegisterView.as_view(),
         name='register',
     ),
+    path(
+        'login/',
+        TokenObtainPairView.as_view(),
+        name='login',
+    ),
+    path(
+        'me/',
+        MeView.as_view(),
+        name='me',
+    ),
+    path(
+    'token/refresh/',
+    TokenRefreshView.as_view(),
+    name='token_refresh',
+),
+
 ]
--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -1,4 +1,7 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import RegisterSerializer
 
@@ -6,4 +9,16 @@ from .serializers import RegisterSerializer
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
 
-# Create your views here.
+
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            'id': request.user.id,
+            'email': request.user.email,
+            'first_name': request.user.first_name,
+            'last_name': request.user.last_name,
+            'college': request.user.college,
+            'graduation_year': request.user.graduation_year,
+        })
